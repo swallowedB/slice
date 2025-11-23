587132083039d3e98b1bc2fb670e635e7742acbc
choiboa <musamea99@gmail.com>
2025-11-23 22:21:53 +0900

    💄 Style : 모달 공통 백그라운드 컴포넌트 제작

---
+++ src/app/components/common/ModalBackground.tsx
@@ -0,0 +1,8 @@
+export default function ModalBackground({onClick}:{onClick?: () => void}) {
+  return (
+    <div 
+      className="fixed inset-0 bg-black/60 z-900" 
+      onClick={onClick}
+    />
+  )
+}
