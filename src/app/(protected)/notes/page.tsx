4df9ee63389930c30013b65ebbc9196af2e18045
jinhee <jinhee@jinheeui-MacBookAir.local>
2025-11-28 10:54:59 +0900

    💄 Style : NotePage에 아이콘 적용

--- src/app/(protected)/notes/page.tsx
+++ src/app/(protected)/notes/page.tsx
@@ -7,7 +7,11 @@ export default async function NotesPage() {
         노트 모아보기
       </h2>
       <div className="mb-4 flex items-center gap-3 rounded-2xl bg-orange-50 p-4 sm:mb-6 sm:rounded-3xl sm:p-6 lg:mb-5 lg:gap-6 lg:p-10">
-        <div className="h-8 w-8 border lg:h-10 lg:w-10"></div>
+        <img
+          src="/icons/icon-goal.svg"
+          alt="목표 아이콘"
+          className="h-8 w-8 lg:h-10 lg:w-10"
+        />
         <div className="text-gray-750 min-w-0 flex-1 truncate text-base font-semibold sm:text-xl lg:text-2xl">
           자바스크립트로 웹 서비스 만들기
         </div>
