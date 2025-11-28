e59e2d9134abecc8f1dd0e41b4eed3d0b4a6d748
jinhee <jinhee@jinheeui-MacBookAir.local>
2025-11-28 16:57:58 +0900

    💄 Style : 공통 TextButton UI 컴포넌트 추가

---
+++ src/components/common/button/TextButton.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+
+interface TextButtonProps {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary";
+  isDisabled?: boolean;
+  className?: string;
+  onClick: () => void;
+}
+
+export default function TextButton({
+  children,
+  variant = "primary",
+  isDisabled = false,
+  className,
+  onClick,
+}: TextButtonProps) {
+  const buttonClasses = clsx(
+    "cursor-pointer text-xs font-semibold sm:text-sm lg:text-base",
+
+    !isDisabled && {
+      "text-orange-250 hover:text-orange-400": variant === "primary",
+      "text-gray-600 hover:text-gray-650": variant === "secondary",
+    },
+
+    isDisabled && "pointer-events-none cursor-not-allowed text-gray-400",
+
+    className,
+  );
+
+  return (
+    <button
+      className={buttonClasses}
+      disabled={isDisabled}
+      onClick={onClick}>
+      {children}
+    </button>
+  );
+}
