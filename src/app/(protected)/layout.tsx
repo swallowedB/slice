3d5a9bc9ed89aad768d1625c98f8ccf5f5af47e5
jjinheeWorld <wlsgml450@naver.com>
2025-12-02 17:02:56 +0900

    💄 Style : ProtectedLayout 퍼블 수정

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -3,12 +3,12 @@ import NavigationDesktop from "./_components/navigation/Navigation.Desktop";
 
 export default function ProtectedLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex h-screen sm:gap-12 lg:gap-20 overflow-hidden">
+    <main className="flex h-screen overflow-hidden sm:gap-12 lg:gap-20">
       <div className="hidden sm:block">
         <NavigationDesktop />
       </div>
 
-      <section className="flex-1 overflow-y-auto px-5 py-12 lg:py-20 lg:pr-40">
+      <section className="flex-1 overflow-y-auto px-5 pt-22 pb-12 sm:pt-12 lg:py-20 lg:pr-40">
         {children}
       </section>
     </main>
