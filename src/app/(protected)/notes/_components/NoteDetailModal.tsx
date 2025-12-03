9593ca8b32db89e7d679affc1f64978e557635a0
jjinheeWorld <wlsgml450@naver.com>
2025-12-04 01:00:15 +0900

    ✨ Feature : NoteDetailModal 데스크탑 전용 슬라이드 인/아웃 애니메이션 적용

--- src/app/(protected)/notes/_components/NoteDetailModal.tsx
+++ src/app/(protected)/notes/_components/NoteDetailModal.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import ModalBackground from "@/components/common/popup-modal/ModalBackground";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import NoteTitleView from "./NoteTitleView";
 import Badge from "./Badge";
+import { useEffect, useState } from "react";
+import { useDeviceSize } from "@/hooks/useDeviceSize";
+import clsx from "clsx";
 
 export default function NoteDetailModal({
   isOpen,
@@ -12,15 +17,51 @@ export default function NoteDetailModal({
   noteId: number | null;
   onClose: () => void;
 }) {
-  if (!isOpen) return null;
+  const [isAnimating, setIsAnimating] = useState(false);
+  const { isDesktop } = useDeviceSize();
+
+  const handleClose = () => {
+    setIsAnimating(false);
+
+    if (!isDesktop) {
+      return onClose();
+    }
+
+    setTimeout(onClose, 300);
+  };
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | null = null;
+
+    if (isOpen) {
+      timerId = setTimeout(() => setIsAnimating(true), 10);
+    } else {
+      setIsAnimating(false);
+    }
+
+    return () => {
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+    };
+  }, [isOpen]);
+
+  if (!isOpen && !isAnimating) return null;
+
   return (
     <>
       <ModalBackground />
-      <div className="fixed inset-y-0 right-0 left-0 z-1000 bg-white px-4 pt-12 pb-4 sm:px-10 sm:pt-18 sm:pb-10 lg:left-auto lg:w-200 lg:rounded-tl-4xl lg:rounded-bl-4xl lg:pt-20">
+      <div
+        className={clsx(
+          "fixed inset-y-0 right-0 left-0 z-1000 bg-white px-4 pt-12 pb-4",
+          "translate-x-0 transition-transform duration-300 ease-out",
+          "sm:px-10 sm:pt-18 sm:pb-10 lg:left-auto lg:w-5xl lg:rounded-tl-4xl lg:rounded-bl-4xl lg:pt-20",
+          !isAnimating && "lg:translate-x-full",
+        )}>
         <button
           type="button"
           aria-label="닫기"
-          onClick={() => onClose()}
+          onClick={handleClose}
           className="absolute top-4 right-4 cursor-pointer text-gray-400 sm:top-10 sm:right-10">
           <XMarkIcon
             strokeWidth={1.8}
