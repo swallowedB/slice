3fb3edb1d31d688b7e4253521baf2f5655d933c5
jjinheeWorld <wlsgml450@naver.com>
2025-12-04 15:01:07 +0900

    🔨 Refactor : NotesPage에 PageHeader 컴포넌트 적용

--- src/app/(protected)/notes/page.tsx
+++ src/app/(protected)/notes/page.tsx
@@ -1,13 +1,13 @@
-import MobileHeader from "../_components/layout/MobileHeader";
+import PageHeader from "../_components/layout/PageHeader";
 import NoteList from "./_components/NoteList";
 
 export default async function NotesPage() {
   return (
     <section className="h-screen">
-      <MobileHeader title="노트 모아보기" />
-      <h2 className="hidden text-black sm:mb-14 sm:block sm:text-2xl sm:font-semibold">
-        노트 모아보기
-      </h2>
+      <PageHeader
+        title="노트 모아보기"
+        className="sm:mb-14"
+      />
       <div className="mb-4 flex items-center gap-3 rounded-2xl bg-orange-50 p-4 sm:mb-6 sm:rounded-3xl sm:p-6 lg:mb-5 lg:gap-6 lg:p-10">
         <img
           src="/icons/icon-goal.svg"
