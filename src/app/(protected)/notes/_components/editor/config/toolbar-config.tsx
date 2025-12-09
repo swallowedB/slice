869fc78a7903ffbdcccd9e1dd7efc7e1a9dfefb8
jjinheeWorld <wlsgml450@naver.com>
2025-12-09 16:44:07 +0900

    💄 Style : Toolbar 아이콘 크기 변경

--- src/app/(protected)/notes/_components/editor/config/toolbar-config.tsx
+++ src/app/(protected)/notes/_components/editor/config/toolbar-config.tsx
@@ -14,19 +14,19 @@ import {
 
 const textStyleOptions: ToolbarConfig[] = [
   {
-    icon: <BoldIcon className="h-5 w-5" />,
+    icon: <BoldIcon className="h-6 w-6" />,
     action: (editor) => editor.chain().focus().toggleBold().run(),
     stateKey: "isBold",
     title: "굵게",
   },
   {
-    icon: <ItalicIcon className="h-5 w-5" />,
+    icon: <ItalicIcon className="h-6 w-6" />,
     action: (editor) => editor.chain().focus().toggleItalic().run(),
     stateKey: "isItalic",
     title: "기울임",
   },
   {
-    icon: <UnderlineIcon className="h-5 w-5" />,
+    icon: <UnderlineIcon className="h-6 w-6" />,
     action: (editor) => editor.chain().focus().toggleUnderline().run(),
     stateKey: "isUnderline",
     title: "밑줄",
@@ -35,19 +35,19 @@ const textStyleOptions: ToolbarConfig[] = [
 
 const alignOptions: ToolbarConfig[] = [
   {
-    icon: <AlignLeftIcon className="h-5 w-5" />,
+    icon: <AlignLeftIcon className="h-6 w-6" />,
     action: (editor) => editor.chain().focus().setTextAlign("left").run(),
     stateKey: "isAlignLeft",
     title: "왼쪽 정렬",
   },
   {
-    icon: <AlignCenterIcon className="h-5 w-5" />,
+    icon: <AlignCenterIcon className="h-6 w-6" />,
     action: (editor) => editor.chain().focus().setTextAlign("center").run(),
     stateKey: "isAlignCenter",
     title: "가운데 정렬",
   },
   {
-    icon: <AlignRightIcon className="h-5 w-5" />,
+    icon: <AlignRightIcon className="h-6 w-6" />,
     action: (editor) => editor.chain().focus().setTextAlign("right").run(),
     stateKey: "isAlignRight",
     title: "오른쪽 정렬",
@@ -56,13 +56,13 @@ const alignOptions: ToolbarConfig[] = [
 
 const listOptions: ToolbarConfig[] = [
   {
-    icon: <ListBulletIcon className="h-5 w-5" />,
+    icon: <ListBulletIcon className="h-6 w-6" />,
     action: (editor) => editor.chain().focus().toggleBulletList().run(),
     stateKey: "isBulletList",
     title: "글머리 기호",
   },
   {
-    icon: <NumberedListIcon className="h-5 w-5" />,
+    icon: <NumberedListIcon className="h-6 w-6" />,
     action: (editor) => editor.chain().focus().toggleOrderedList().run(),
     stateKey: "isOrderedList",
     title: "번호 매기기",
@@ -71,7 +71,7 @@ const listOptions: ToolbarConfig[] = [
 
 const highlightOptions: ToolbarConfig[] = [
   {
-    icon: <HighlightIcon className="h-5 w-5" />,
+    icon: <HighlightIcon className="h-6 w-6" />,
     action: (editor) => editor.chain().focus().toggleHighlight().run(),
     stateKey: "isHighlight",
     title: "형광펜",
