13539fd0f23154befaf3a2264e7f5d55fb5b0a16
choiboa <musamea99@gmail.com>
2025-12-11 10:09:54 +0900

    ✨ Feature : AuthGuard 기능 추가

---
+++ src/store/useAuthStore.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { AuthUser } from "@/hooks/queries/auth/queryKeys";
+
+interface AuthState {
+  user: AuthUser | null;
+  hydrated: boolean;
+  setUser: (user: AuthUser) => void;
+  clearUser: () => void;
+  setHydrated: (value: boolean) => void;
+}
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      hydrated: false,
+      setUser: (user) => set({ user }),
+      clearUser: () => set({ user: null }),
+      setHydrated: (value) => set({ hydrated: value }),
+    }),
+    {
+      name: "slice_auth", 
+      partialize: (state) => ({ user: state.user }),
+      onRehydrateStorage: () => (state) => {
+        state?.setHydrated(true);
+      },
+    }
+  )
+);
