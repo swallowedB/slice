be67dbb5c63240b34a2b2b451a7d27b99381f4c6
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2025-12-13 13:17:31 +0900

    ✨ Feature : 목표상세 할일들과 목표 없을 때, 할일들 없고 목표만 있을 때 구현

--- src/app/(protected)/(dashboard)/_components/goal/Goal.tsx
+++ src/app/(protected)/(dashboard)/_components/goal/Goal.tsx
@@ -4,36 +4,17 @@ import EmptyState from "@/components/common/empty-state/EmptyState";
 import GoalCard from "./GoalCard";
 import { EMPTY_MESSAGES } from "@/constants/messages";
 import { useTodoList } from "../../../_hooks/useTodoList";
-import { ListGoalType } from "@/components/common/list/list-item/types";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useGoalList } from "@/hooks/queries/goals";
 
 const cardStyles =
   "relative mt-4 rounded-[28px] bg-white px-4 py-6.5 pb-11 shadow-[0_4px_4px_0_rgba(0,0,0,0.025)] transition-all sm:p-4 sm:pt-7.5 lg:rounded-[40px] lg:p-7.5 lg:hover:shadow-[0_4px_4px_0_rgba(0,0,0,0.025)]";
 
 export default function Goal() {
   const { todos } = useTodoList();
-
-  const groupedGoals: ListGoalType[] = Object.values(
-    todos.reduce(
-      (acc, cur) => {
-        const goal = cur.goal!;
-        if (!acc[goal.id]) {
-          acc[goal.id] = {
-            id: goal.id,
-            title: goal.title,
-            todos: [],
-          };
-        }
-        acc[goal.id].todos.push(cur);
-        return acc;
-      },
-      {} as Record<number, ListGoalType>,
-    ),
-  );
-  const goalGroups = Object.values(groupedGoals);
+  const { data } = useGoalList();
 
   const [openCards, setOpenCards] = useState<Record<number, boolean>>({});
-
   const onToggleCard = (id: number) => {
     setOpenCards((prev) => {
       const current = prev[id];
@@ -46,38 +27,40 @@ export default function Goal() {
     });
   };
 
+  const hasNoGoals = !data || data.goals.length === 0;
   return (
     <section>
       <h3 className="mb-3 pl-2 text-lg font-medium">목표별 할 일</h3>
 
-      {goalGroups.length === 0 ? (
+      {hasNoGoals && (
         <div className="rounded-[28px] bg-white p-4 shadow">
           <EmptyState>{EMPTY_MESSAGES.GOAL.RECENT}</EmptyState>
         </div>
-      ) : (
-        goalGroups.map((group) => {
-          const todoItems = group.todos.filter((t) => !t.checked);
-          const doneItems = group.todos.filter((t) => t.checked);
-
-          const percent =
-            group.todos.length === 0
-              ? 0
-              : Math.floor((doneItems.length / group.todos.length) * 100);
-
-          return (
-            <GoalCard
-              key={group.id}
-              title={group.title}
-              percent={percent}
-              todoItems={todoItems}
-              doneItems={doneItems}
-              isOpen={openCards[group.id] ?? true}
-              onToggle={() => onToggleCard(group.id)}
-              cardStyles={cardStyles}
-            />
-          );
-        })
       )}
+      {data?.goals?.map((goal) => {
+        const goalTodos = todos.filter((todo) => todo.goal?.id === goal.id);
+
+        const todoItems = goalTodos.filter((t) => !t.checked);
+        const doneItems = goalTodos.filter((t) => t.checked);
+
+        const percent =
+          goalTodos.length === 0
+            ? 0
+            : Math.floor((doneItems.length / goalTodos.length) * 100);
+
+        return (
+          <GoalCard
+            key={goal.id}
+            title={goal.title}
+            percent={percent}
+            todoItems={todoItems}
+            doneItems={doneItems}
+            isOpen={openCards[goal.id] ?? true}
+            onToggle={() => onToggleCard(goal.id)}
+            cardStyles={cardStyles}
+          />
+        );
+      })}
     </section>
   );
 }
