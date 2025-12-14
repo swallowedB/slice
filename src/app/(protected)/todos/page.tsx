84b48355a958dde79722d43a27c76eb16db1edb4
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-12-15 04:51:23 +0900

    🔨 Refactor: 모든 할 일 상단 타이틀 및 할일추가 버튼 공통 layout과 컴포넌트로 변경

--- src/app/(protected)/todos/page.tsx
+++ src/app/(protected)/todos/page.tsx
@@ -1,20 +1,22 @@
 "use client";
 
+import { useState, Suspense } from "react";
+import PageHeader from "../_components/layout/PageHeader";
 import TodoHeader from "./_components/TodoHeader";
 import TodosContent from "./_components/TodosContent";
-import { useState } from "react";
 import TodosLayout from "./_components/TodosLayout";
 import MobileHeader from "@/app/(protected)/_components/layout/MobileHeader";
-import { Suspense } from "react";
 
 export default function TodosPage() {
   const [tab, setTab] = useState<"ALL" | "TODO" | "DONE">("ALL");
 
   return (
-    <section>
-      <MobileHeader title="모든 할 일" />
+    <section className="h-screen">
       <h2 className="color-black hidden sm:block sm:pl-4 sm:text-2xl sm:font-semibold">
-        모든 할 일
+        <PageHeader
+          title="모든 할 일"
+          desktopClassName="sm:mb-2"
+        />
       </h2>
       <TodosLayout>
         <TodoHeader
