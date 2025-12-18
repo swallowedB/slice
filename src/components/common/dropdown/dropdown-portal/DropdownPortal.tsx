828e462af3baf9d6dbf002b8701658d665c70e70
iyxxnjin <131842947+iyxxnjin@users.noreply.github.com>
2025-12-18 11:56:05 +0900

    🔨 Refactor: dropdown portal 적용 모바일 버전에서 위치 수정

--- src/components/common/dropdown/dropdown-portal/DropdownPortal.tsx
+++ src/components/common/dropdown/dropdown-portal/DropdownPortal.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { useDeviceSize } from "@/hooks/useDeviceSize";
+
 import Portal from "../../../../components/common/dropdown/dropdown-portal/Portal";
 import Dropdown, { DropdownItem } from "@/components/common/dropdown";
 
@@ -13,17 +15,27 @@ export default function DropdownPortal({
   onClose,
 }: DropdownPortalProps) {
   const [style, setStyle] = useState<React.CSSProperties>({});
+  const { isMobile } = useDeviceSize();
 
   useEffect(() => {
     if (!anchorRect) return;
 
-    setStyle({
-      position: "absolute",
-      top: anchorRect.bottom + window.scrollY + 4,
-      left: anchorRect.right + window.scrollX - 20,
-      zIndex: 1000,
-    });
-  }, [anchorRect]);
+    if (isMobile) {
+      setStyle({
+        position: "fixed",
+        top: anchorRect.bottom + window.scrollY + 4,
+        left: anchorRect.right + window.scrollX - 90,
+        zIndex: 1000,
+      });
+    } else {
+      setStyle({
+        position: "absolute",
+        top: anchorRect.bottom + window.scrollY + 4,
+        left: anchorRect.right + window.scrollX - 20,
+        zIndex: 1000,
+      });
+    }
+  }, [anchorRect, isMobile]);
 
   return (
     <Portal>
