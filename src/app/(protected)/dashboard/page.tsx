f68602466a9689bad80b15d26a21df69e8898a65
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2025-12-31 08:40:26 +0900

    🗑️ Remove : 에러바운더리 파일 삭제

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { Suspense } from "react";
 import DashboardClient from "./_components/DashboardClient";
 
 export const dynamic = "force-dynamic";
 
 export default function DashboardPage() {
-  return <DashboardClient />;
+  return (
+    <Suspense>
+      <DashboardClient />
+    </Suspense>
+  );
 }
