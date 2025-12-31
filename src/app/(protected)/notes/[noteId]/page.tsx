f8445e990915f39154388a9212f64bf166b8c035
jjinheeWorld <wlsgml450@naver.com>
2025-12-31 16:49:21 +0900

    🔨Refactor: 노트 상세 전체 페이지에 BFF backendFetch, React Query Hydration 적용

--- src/app/(protected)/notes/[noteId]/page.tsx
+++ src/app/(protected)/notes/[noteId]/page.tsx
@@ -1,7 +1,15 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { backendFetch } from "@/lib/backend";
+import { NoteDetailResponse } from "@/api/types/note";
+import notesQueryKeys from "@/hooks/queries/notes/queryKeys";
+import { AsyncBoundary } from "@/app/(protected)/_components/AsyncBoundary";
+import PageHeader from "@/app/(protected)/_components/layout/PageHeader";
 import NoteDetailContent from "@/app/(protected)/notes/_components/NoteDetailContent";
-import PageHeader from "../../_components/layout/PageHeader";
-import { AsyncBoundary } from "../../_components/AsyncBoundary";
-import NoteDetailSkeleton from "../_components/NoteDetailSkeleton";
+import NoteDetailSkeleton from "@/app/(protected)/notes/_components/NoteDetailSkeleton";
 
 export default async function NoteDetailPage({
   params,
@@ -11,18 +19,29 @@ export default async function NoteDetailPage({
   }>;
 }) {
   const { noteId } = await params;
+  const noteIdNumber = Number(noteId);
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: notesQueryKeys.detail(noteIdNumber),
+    queryFn: () =>
+      backendFetch<NoteDetailResponse>(`/notes/${noteIdNumber}`, {
+        auth: "access",
+      }),
+  });
 
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <PageHeader
         title="노트 상세보기"
         desktopClassName="sm:mb-3 lg:mb-5.5"
       />
       <div className="min-h-[75vh] rounded-4xl bg-white p-4 sm:min-h-[80vh] sm:p-8">
         <AsyncBoundary loadingFallback={<NoteDetailSkeleton />}>
-          <NoteDetailContent noteId={Number(noteId)} />
+          <NoteDetailContent noteId={noteIdNumber} />
         </AsyncBoundary>
       </div>
-    </>
+    </HydrationBoundary>
   );
 }
